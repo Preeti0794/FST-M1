77d10ec53de6095f655ed09bc2f548e96beea2dd
Preeti0794 <67922926+Preeti0794@users.noreply.github.com>
2022-02-02 11:16:00 +0530

    Activity 4-2 completed
    Activity 4-2  has been completed. Uploaded the code into repo.

--- Selenium/Activities/Activity4.java
+++ Selenium/Activities/Activity4.java
@@ -9,13 +9,17 @@ public class Activity4 {
 		 WebDriver driver = new FirefoxDriver();
 			
 	        // Open the browser
-	        driver.get("https://www.training-support.net");
+	        driver.get(" https://www.training-support.net/selenium/simple-form");
 	        String title = driver.getTitle();
 	        System.out.println("Title of the application::" +title);
 	        
-	        driver.findElement(By.xpath("//*[@id='about-link']")).click();
-	        String new_Title = driver.getTitle();
-	        System.out.println("Title of new tab::" +new_Title);
+	        driver.findElement(By.xpath("//*[@id='firstName']")).sendKeys("Charles");
+	        driver.findElement(By.xpath("//*[@id='lastName']")).sendKeys("John");
+	        driver.findElement(By.xpath("//*[@id='email']")).sendKeys("charles89@gmail.com");
+	        driver.findElement(By.xpath("//*[@id='number']")).sendKeys("8789098900");
+	        //Click on submit button
+	        driver.findElement(By.xpath("//input[@type='submit']")).click();
+	        System.out.println("Form submitted successfully using xpath");
 	        driver.close();
 	}
 
